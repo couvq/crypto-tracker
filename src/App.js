87fb4beb4e506f0c7c8961b3c002453ecbbf42a5
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-15 16:49:13 -0500

    unable to finish because react-router-dom useHistory is deprecated, darn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { makeStyles } from "@material-ui/core";
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import './App.css';
+import Header from "./components/Header";
+import CoinPage from "./pages/CoinPage";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
 
+    const useStyles = makeStyles(() => ({
+        App: {
+            backgroundColor: '#14161a',
+            color: 'white',
+            minHeight: '100vh'
+        }
+    }));
+
+    const classes = useStyles();
+
     return (
-        <div>
-            Hello World
-        </div>
+        <BrowserRouter>
+            <div className={classes.App}>
+                <Header /> 
+                <Route path="/" component={Homepage} exact />
+                <Route path="/coins/:id" component={CoinPage} />
+            </div>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const useStyles = makeStyles(() => ({
+    title: {
+        flex: 1,
+        color: 'gold',
+        fontFamily: 'Montserrat',
+        fontWeight: 'bold',
+        cursor: 'pointer'
+    }
+}));
+
+const Header = () => {
+
+    const classes = useStyles();
+
+    const history = useHistory();
+
+  return (
+    <AppBar color='transparent' position='static'>
+        <Container>
+            <Toolbar>
+                <Typography onClick={() => history.push("/")} className={classes.title}>
+                    Crypto Tracker
+                </Typography>
+
+                <Select variant='outlined' style={{
+                    width: 100,
+                    height: 40,
+                    marginLeft:15
+                }}>
+                    <MenuItem value={'USD'}>USD</MenuItem>
+                    <MenuItem value={'INR'}>INR</MenuItem>
+                </Select>
+            </Toolbar>
+        </Container>
+    </AppBar>
+  );
+}
+
+export default Header;
